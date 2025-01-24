67fe749a15e96e40d3da5a84fe11f4f9d09832d8
Andrew Kosarev <kosarev-2004@mail.ru>
2025-01-24 03:31:43 +0300

    comm

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  root: 'frontend', // Указываем корневую директорию
   build: {
-    outDir: 'dist',
+    outDir: '../dist', // Путь для выходных файлов относительно директории root
     emptyOutDir: true,
   },
   server: {
